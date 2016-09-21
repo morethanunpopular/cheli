afb6f1135acb1c032624522e1dc6493024dce369
root <grant@grantcampbell.co>
2016-09-21 22:02:21 +0000

    added command template

---
+++ modules/template/Template.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+class execute:
+  def __init__(self,arg,option):
+  #the bare minimum for a tool in Cheli is that it has a class called "execute"
+  # in this init of this class, we need all operations that are to be preformed when the tool is used
