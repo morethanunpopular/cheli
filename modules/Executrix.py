f292efcd7b1fb2c39e7b5e0b251cfdd9a9036e42
root <grant@grantcampbell.co>
2016-09-22 03:04:20 +0000

    added some simple validation to the structure of an imported command

--- modules/Executrix.py
+++ modules/Executrix.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
-
+import sys
 def RunCommand(command,args,options):
   moduleString = "{0}.{0}".format(command)
-  commandObject = __import__(moduleString, globals(), locals(), ['execute'], -1)  
+  commandObject = __import__(moduleString, globals(), locals(), ['execute'], -1) 
+  try:
+    commandObject.__dict__['execute'].__dict__
+  except:
+    print "[!!] Fatal Error! Command has no execute class"
+    sys.exit()
   commandObject.execute(args,options)
